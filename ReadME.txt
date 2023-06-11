42a8ec17498b422012d635117b0f3ea273785f3c
AAROMAL-ANIL-001 <aaromalanil001@gmail.com>
2023-06-11 23:41:50 +0530

    readme  + local string added

---
+++ ReadME.txt
@@ -0,0 +1,8 @@
+run commands
+
+frontend - Woxro Task\frontend> npm run dev
+backend - Woxro Task\backend> nodemon server.js
+
+#use local connection string -   "mongodb://localhost:27017/woxro"
+
+Everything works as fine as task provided - if any doubts can contact me - 7907335059 / aaromalanil001@gmail.com
--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,9 @@ app.use(express.json());
 
 const port = 5000;
 
+//use local connection string
 mongoose
-  .connect(
-    "mongodb+srv://aaromalanil001:Upscarml123@cluster1.akcuv97.mongodb.net/woxro"
-  )
+  .connect("mongodb://localhost:27017/woxro") //connection string goes here
   .then(() => console.log("Db connected"))
   .catch((err) => console.log("error in db", err));
 
